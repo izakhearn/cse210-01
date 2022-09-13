6600eecfbea7db31ba311f47f1a4aa111df00729
Izak Hern <izakhearn@gmail.com>
2022-09-13 18:39:26 +0200

    Added Player 1 Win and Player 2 Win Test
    In this commit I added 2 new tests. 
    There two test are responsible for checking 2 win scenarios for player 1 and player 2. 
    
    This essentially ensures that we calculate the correct winner of the game each time.

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 from tick_tack import *
 import pytest
+import sys
+from io import StringIO
 
 # Test Check Board Size Function
 def test_check_board_size():
@@ -15,6 +17,24 @@ def test_check_symbols():
     assert checkSymbols("X", "O") == True
     # Test that the symbols are invalid
     assert checkSymbols("X", "X") == False
+
+#Test Player 1 Win Situation
+def test_player1_win():
+    #Create a new instance of the draw board class
+    board = db.DrawBoard(3, 3, "Player 1", "Player 2", "X", "O")
+    sys.stdin = StringIO("0,0 \n 0,1 \n 1,0 \n 1,1 \n 2,0")
+    board.getMoves()
+    assert board.player1Win == True
+
+
+#Test Player 2 Win Situation
+def test_player2_win():
+    #Create a new instance of the draw board class
+    board = db.DrawBoard(3, 3, "Player 1", "Player 2", "X", "O")
+    sys.stdin = StringIO("2,2 \n 0,0 \n 1,0 \n 0,1 \n 2,0 \n 0,2")
+    board.getMoves()
+    assert board.player2Win == True
+
 
 #Run the Main Test
 pytest.main(["-v", "--tb=line", "-rN", __file__])
